5f0e76d7f77fa138ca4de917a480e196132aa310
Jayant Khandelwal <jayant.khandelwal7@gmail.com>
2020-07-18 17:06:17 +0530

    refactor

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/statistics', function(req, res, next) {
+  res.render('statistics');
+});
+
+router.get('/map', function(req, res, next) {
+  res.render('map');
+});
+
+router.get('/news', function(req, res, next) {
+  res.render('news');
+});
+
+router.get('/description', function(req, res, next) {
+  res.render('description');
+});
+
+
+
 module.exports = router;
